2b7498aebc47484446596c34c6e2c94d0ea4bd3e
ahananupam33 <68801494+ahananupam33@users.noreply.github.com>
2020-11-19 15:24:57 +0530

    Create ANDOR.cpp

---
+++ Codechef_Contests/October_LunchTime/ANDOR.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main() {
+    ll int T,x;
+    cin >> T;
+    for(int i=0;i<T;i++)
+    {
+        cin >> x;
+        cout << 0 << " " << x << endl;
+    }
+	// your code goes here
+	return 0;
+}
